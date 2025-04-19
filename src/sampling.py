5289fc0aad816720c711af5e46b09059fcf23f17
YI WEN CHAN <v41827@gmail.com>
2025-04-19 21:37:14 +0100

    add abc, abstractmethod

--- src/sampling.py
+++ src/sampling.py
@@ -1,16 +1,19 @@
 # sampling.py
 
-class Sampling: #parent class
+from abc import ABC, abstractmethod
+
+class Sampling(ABC):  # parent class
     def __init__(self, video, num_frames):
         self.video = video
         self.num_frames = num_frames
 
+    @abstractmethod
     def sample(self):
-        raise NotImplementedError("Each subclass must implement this method.")
+        pass
 
 class EquidistantSampling(Sampling):
     def sample(self):
-        # implement equidistant sampling across the eentire video here
+        # implement equidistant sampling across the entire video here
         pass
 
 class InterpolationSampling(Sampling):
